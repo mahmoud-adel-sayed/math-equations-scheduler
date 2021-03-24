1787d1fbea9b116a662a3bc091aabc20c44cc558
mahmoudadelsayed <mahmoudtaha939@gmail.com>
2021-03-24 12:50:36 +0200

    Added Unit Tests for the TimeUtil class.

---
+++ app/src/test/java/com/va/android/task/implementation/java/util/TimeUtilParameterizedTest.kt
@@ -0,0 +1,35 @@
+package com.va.android.task.implementation.java.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
+@RunWith(Parameterized::class)
+class TimeUtilParameterizedTest(
+        private val hours: Long,
+        private val minutes: Long,
+        private val seconds: Long,
+        private val expectedFormattedTime: String
+) {
+    companion object {
+        @JvmStatic
+        @Parameters(name= "{index}: TimeUtil.getTimeFormatted({0}, {1}, {2}) = {3}")
+        fun data(): Iterable<Array<Any>> = listOf(
+                arrayOf(0, 0, 0, "00:00:00"),
+                arrayOf(1, 0, 0, "01:00:00"),
+                arrayOf(0, 2, 0, "00:02:00"),
+                arrayOf(0, 0, 10, "00:00:10"),
+                arrayOf(1, 1, 9, "01:01:09"),
+                arrayOf(10, 10, 10, "10:10:10"),
+                arrayOf(11, 12, 13, "11:12:13")
+        )
+    }
+
+    @Test
+    fun getTimeFormatted_returnsCorrectFormat() {
+        val time = TimeUtil.getTimeFormatted(hours, minutes, seconds)
+        assertEquals(time, expectedFormattedTime)
+    }
+}
---
+++ app/src/test/java/com/va/android/task/implementation/java/util/TimeUtilTest.kt
@@ -0,0 +1,40 @@
+package com.va.android.task.implementation.java.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TimeUtilTest {
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getTimeFormatted_withNegativeHours_throwsException() {
+        TimeUtil.getTimeFormatted(-1, 0, 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getTimeFormatted_withNegativeMinutes_throwsException() {
+        TimeUtil.getTimeFormatted(0, -1, 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun getTimeFormatted_withNegativeSeconds_throwsException() {
+        TimeUtil.getTimeFormatted(0, 0, -1)
+    }
+
+    @Test
+    fun getTimeFormatted_returnsCorrectFormat() {
+        val time = TimeUtil.getTimeFormatted(8, 6, 5)
+        assertEquals(time, "08:06:05")
+    }
+
+    @Test
+    fun getDigits_returnsCorrectFormat() {
+        var digits = TimeUtil.getDigits(9)
+        assertEquals(digits, "09")
+
+        digits = TimeUtil.getDigits(10)
+        assertEquals(digits, "10")
+
+        digits = TimeUtil.getDigits(15)
+        assertEquals(digits, "15")
+    }
+}
